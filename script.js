0acb190546a9a700727e4bf82c0a730b45f12f49
HuseynAlizade <huseynalizade2010@gmail.com>
2024-08-06 10:56:45 +0400

    js2h-w

--- script.js
+++ script.js
@@ -15,20 +15,21 @@
 //var age = prompt("How old are you?")
 
 //if (age >= 20){
- //   alert(age + " Logged in successfully");
+//    alert(age + " Logged in successfully");
 //} else {
 //alert(age + " Please wait for your 20 age");
-// }
+ //}
 
-//?task 3
+
+// ?task 3
 
 //var age = prompt("Please enter your age")
 
 //if (age < 20){
-//   alert(age + " Please wait for your 20 age");
-// } else if (age >= 20 && age <65){
-  //  alert(age + "You are accepted");
+//  alert(age + " Please wait for your 20 age");
+//} else if (age >= 20 && age <=65){
+  // alert(age + "You are accepted");
  //}else{
-  //  alert(age + " Your are older for job");
+ //  alert(age + " Your are older for job");
  //}
 
